9901631a77e8b0302b9f846c378b18f3eba5d5b6
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2024-11-07 02:05:45 +0600

    add 7

---
+++ src/problem_7.ts
@@ -0,0 +1,17 @@
+class Car {
+  make: string;
+  model: string;
+  year: number;
+
+  constructor(make: string, model: string, year: number) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+  getCarAge() {
+    let currentYear = new Date().getFullYear();
+    return currentYear - this.year;
+  }
+}
+// const myCar = new Car("Honda", "civic", 2018);
+// console.log(`${myCar.getCarAge()} (assuming current year is 2024)`);
